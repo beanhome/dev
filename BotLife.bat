bf34d86c58de683f8dcf42e0cc9f75ad40d09eba
beanhome <quentin.richet@gmail.com>
2013-03-09 22:18:39 +0100

    [BotLife] Ajout de l'exe de BotLife et des batch pour le lancer

---
+++ BotLife.bat
@@ -0,0 +1,2 @@
+WorkingDir\BotLife\BotLife.exe -a BLApp -w 920 -h 800 -r 0.54 -x 32 -y 32 -t summertiles
+
---
+++ WorkingDir/BotLife/BotLife.bat
@@ -0,0 +1,2 @@
+BotLife.exe -a BLApp -w 920 -h 800 -p ../../ -r 0.54 -x 32 -y 32 -t summertiles
+
